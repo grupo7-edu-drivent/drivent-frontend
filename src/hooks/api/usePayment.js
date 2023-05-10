3864ba50bf8c4c52e4ad98d9a73e6868c7eaa3da
Tadeuvelloso <tadeuvellososilva@gmail.com>
2023-05-10 20:22:00 -0300

    feat: getTyckets hook

---
+++ src/hooks/api/usePayment.js
@@ -0,0 +1,22 @@
+import useAsync from '../useAsync';
+import useToken from '../useToken';
+
+import * as paymentApi from '../../services/paymentApi';
+
+export default function useTicketTypes() {
+  const token = useToken();
+  
+  const {
+    data: tycketsType,
+    loading: tycketsTypeLoading,
+    error: tycketsTypeError,
+    act: getTycketsType
+  } = useAsync(() => paymentApi.getTicketTypes(token));
+
+  return {
+    tycketsType,
+    tycketsTypeLoading,
+    tycketsTypeError,
+    getTycketsType
+  };
+}
