309f3685c8e00d3e24a977ed19f121579657636f
Tadeuvelloso <tadeuvellososilva@gmail.com>
2023-05-11 22:09:51 -0300

    feat: request tratament

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -2,51 +2,68 @@
 import styled from 'styled-components';
 import PaymentButton from '../../../layouts/PaymentButton';
 import { useState } from 'react';
-
+import Button from '../../../components/Form/Button';
 import useTicketTypes from '../../../hooks/api/usePayment';
+import { useEffect } from 'react';
 
 export default function Payment() {
-  const [select, setSelect] = useState(false);
   const [ticket, setTicket] = useState({});
-  const [remote, setRemote] = useState({});
-  const [inPerson, setInPerson] = useState([]);
+  const [select, setSelect] = useState();
+  // const [remote, setRemote] = useState([]);
+  // const [inPerson, setInPerson] = useState([]);
+  // const [ticketTypeData, setTicketTypeData] = useState([]);
 
-  const { tycketsType } = useTicketTypes();
-  console.log(tycketsType);
-  
-  function ticketTypeData(tycketsType) {
-    const isRemote = tycketsType.filter((t) => t.isRemote === true);
-    setRemote(isRemote);
-    const noIsRemote = tycketsType.filter((t) => t.isRemote === false);
-    setInPerson(noIsRemote);
+  const { ticketsType, ticketsTypeLoading } = useTicketTypes();
+  if (ticketsTypeLoading) { return <h1>Loaging</h1>; }
+  console.log(ticketsType);
+
+  const remote = [];
+  const inPerson = [];
+
+  for (const item of ticketsType) {
+    if (item.isRemote) {
+      remote.push(item);
+    } else {
+      inPerson.push(item);
+    }
   }
+
+  const withHotel = inPerson.filter((i) => i.includesHotel);
+  const noHotel = inPerson.filter((i) => !i.includesHotel);
+
   console.log(remote);
   console.log(inPerson);
-  // function featTicket() {
-
-  // }
-
-  const value = 250;
-  const title = 'presencial'; 
 
   return (
     <Main>
       <h1>Ingressos e pagamento</h1>
-      <h2>Primeiro, escolha sua modalidade de ingresso</h2>
-      <ButtonsContainer>
-        <PaymentButton  price={value} subtitle={title} selected={select} onClick={ticketTypeData}/>
-        <PaymentButton price={value} subtitle={title} selected={select} onClick={ticketTypeData}/>
-      </ButtonsContainer>
+      <ManagementButton data={[noHotel[0].price, remote[0].price, setSelect]} />
       <h2>Ótimo! Agora escolha sua modalidade de hospedagem</h2>
       <ButtonsContainer>
-        <PaymentButton  price={value} subtitle={'Com hotel'} selected={select} onClick={() => setSelect(!select)}/>
-        <PaymentButton price={value} subtitle={'Sem Hotel'} selected={select} onClick={() => setSelect(!select)}/>
+        <PaymentButton price={withHotel[0].price - noHotel[0].price} subtitle={'Com hotel'} selected={select} onClick={() => setSelect(!select)} />
+        <PaymentButton price={0} subtitle={'Sem Hotel'} selected={select} onClick={() => setSelect(!select)} />
       </ButtonsContainer>
       <h3>Fechado! O total ficou em R$ {'x'}. Agora é só confirmar:</h3>
-      <ComfirmatioButton>
+      <Button>
         <p>RESERVAR INGRESSO</p>
-      </ComfirmatioButton>
+      </Button>
     </Main>
+  );
+}
+
+function ManagementButton({ data }) {
+  function userChosen(chosen) {
+    console.log('chose');
+    data.setSelect(chosen);
+  }
+  return (
+    <>
+      <h2>Primeiro, escolha sua modalidade de ingresso</h2>
+      <ButtonsContainer>
+        <PaymentButton price={data[0]} subtitle={'Presencial'} onClick={() => userChosen('Presencial')}/>
+        <PaymentButton price={data[1]} subtitle={'Online'}  onClick={() => console.log('clicado')}/>
+      </ButtonsContainer>
+    </>
   );
 }
 
@@ -77,27 +94,3 @@ const ButtonsContainer = styled.div`
   justify-content: space-between;
 `;
 
-const ComfirmatioButton = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-height: 37px;
-width: 162px;
-background-color: #E0E0E0;
-color: #000000;
-font-size: 10;
-border-radius: 4px;
-margin-top: 10px;
-box-shadow: 4px 4px 1.5px 0.5px lightgray;
-p{
-  background-color: #E0E0E0;
-  color: #000000;
- font-size: 14px;
- font-weight: 500;
-}
-:hover{
-  cursor: pointer;
-  opacity: 0.7;
-  box-shadow: 4px 4px 1.5px 0.5px gray;
-}
-`;
