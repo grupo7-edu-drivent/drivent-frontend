436c6b0e7edec263ff292ea2460eea1f8045bd4f
matheus <wallysom2@gmail.com>
2023-05-26 14:02:42 -0300

    feat: building table with roomtitles

--- src/components/ActivityCard/index.js
+++ src/components/ActivityCard/index.js
@@ -1,55 +1,32 @@
 import { CgEnter } from 'react-icons/cg';
 import styled from 'styled-components';
 
-export const activities = [
-  {
-    id: 1,
-    title: 'Minecraft: montando o PC ideal',
-    startDate: '2023-11-18 11:00',
-    endDate: '2023-11-18 12:00',
-    roomTitle: 'Auditório Principal',
-    capacity: 14,
-    subscribed: false,
-  },
-  {
-    id: 2,
-    title: 'LoL: montando o PC ideal',
-    startDate: '2023-11-18 14:00',
-    endDate: '2023-11-18 16:00',
-    roomTitle: 'Auditório Principal',
-    capacity: 11,
-    subscribed: false,
-  },
-  {
-    id: 3,
-    title: 'Palestra X',
-    startDate: '2023-11-19 09:00',
-    endDate: '2023-11-19 12:00',
-    roomTitle: 'Auditório Lateral',
-    capacity: 40,
-    subscribed: false,
-  },
-  {
-    id: 4,
-    title: 'Palestra Y',
-    startDate: '2023-11-20 10:00',
-    endDate: '2023-11-20 12:00',
-    roomTitle: 'Sala de Workshop',
-    capacity: 16,
-    subscribed: false,
-  },
-];
+export default function ActivityCard({ activity }) {
+  const { title, startDate, endDate, roomTitle, capacity } = activity;
 
-export default function ActivityCard() {
+  const startTime = new Date(startDate).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+  const endTime = new Date(endDate).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
+  const startDateTime = new Date(startDate);
+  const endDateTime = new Date(endDate);
+  const timeDifference = Math.abs(endDateTime - startDateTime);
+  const hoursDifference = Math.ceil(timeDifference / (1000 * 60 * 60));
+  const mainContainerHeight = hoursDifference * 79;
+  const dividerHeight = hoursDifference * 60;
+
   return (
     <>
-      <MainContainer>
+      <MainContainer style={{ height: `${mainContainerHeight}px` }}>
         <TextContainer>
-          <h3>Minecraft: montando o PC ideal</h3>
-          <h4>18/11/2023</h4>
+          <h3>{title}</h3>
+          <h4 style={{ fontWeight: 400, marginTop: '6px' }}>
+            {startTime} - {endTime}
+          </h4>
         </TextContainer>
         <IconContainer>
-          <CgEnter />
+          <Divider style={{ height: `${dividerHeight}px` }} />
+          <CgEnter style={{ fontSize: '20px' }} />
+          <h5 style={{ fontSize: '9px', marginTop: '5px' }}>{capacity} vagas</h5>
         </IconContainer>
       </MainContainer>
     </>
@@ -59,22 +36,41 @@ export default function ActivityCard() {
 const MainContainer = styled.div`
   display: flex;
   flex-direction: row;
-`;
-
-const TextContainer = styled.div`
+  justify-content: space-between;
   width: 275px;
-  height: 79px;
-  background-color: #F1F1F1;
+  background-color: #f1f1f1;
   border-radius: 5px;
   margin: 9px;
-
-  h3 {
+  cursor: pointer;
+  h3,
+  h4 {
     font-family: 'Roboto';
-    font-style: normal;
     font-weight: 700;
     font-size: 12px;
     line-height: 14px;
   }
 `;
 
-const IconContainer = styled.div``;
+const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  margin: 12px;
+`;
+
+const IconContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: #078632;
+  margin-right: 22px;
+  position: relative;
+`;
+
+const Divider = styled.div`
+  position: absolute;
+  left: -20px;
+  width: 1px;
+  background-color: #cfcfcf;
+`;
